994fab35f1fc9d491eeef69ee18430c19bf25818
Sayon-okai <sayon.study@gmail.com>
2025-10-16 13:55:27 +0000

    js logic added - no media query

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
 // Get DOM Content
 let signUpScreen =  document.querySelector(".sign-up-screen");
 let labelErr = document.querySelector(".label-err");
-let errMessage = document.querySelector(".error-msg");
-let validMsg = document.querySelector(".valid-msg");
+let validationMessage = document.querySelector(".message");
 let newsletterEmail = document.querySelector("#email");
 let subscribeButton = document.querySelector("#subscribe-btn");
 let successEmail = document.querySelector(".visitor-email");
@@ -10,22 +9,32 @@ let dismissMsgBtn = document.querySelector("#dismiss-msg");
 let successScreen = document.querySelector(".success-screen");
 
 
+// Update views TODO: remove success screen
+dismissMsgBtn.addEventListener("click", resetViews);     
 
-// 
+// On submit 
 subscribeButton.addEventListener("click", function () {
- 
+//  validations
     if (validateEmail(newsletterEmail.value)) {
-        
+         // Update views 
+        signUpScreen.classList.add("hidden");
+        successScreen.classList.remove("hidden");
+        successEmail.textContent = newsletterEmail.value;
        
-    }  else if (newsletterEmail.value === "") {
-                    validMsg.textContent = "Valid email required";
-                    validMsg.classList.remove("valid-msg")
-                     validMsg.classList.add("error-msg");
+    } else if (newsletterEmail.value === "") {
+        
+                  validationMessage.textContent = "Valid email required"
+                  validationMessage.classList.remove("valid-msg")
+                  validationMessage.classList.add("error-msg");
+                      
             }
     
     else {
-        errMessage.textContent = "Valid email required"
-        document.querySelector(".error-msg").style.display = "none";
+         
+                  validationMessage.textContent = "Valid email required"
+                  validationMessage.classList.remove("valid-msg")
+                  validationMessage.classList.add("error-msg");
+       
     }
 
    
@@ -39,15 +48,15 @@ document.addEventListener("input", function () {
         newsletterEmail.addEventListener("keypress", function () {
          
             if (validateEmail(newsletterEmail.value)) {
-                     validMsg.classList.add("valid-msg");
-                     validMsg.textContent = "Valid email";
-                     validMsg.classList.remove("error-msg");
+                     validationMessage.classList.add("valid-msg");
+                     validationMessage.textContent = "Valid email";
+                     validationMessage.classList.remove("error-msg");
             }
             
             else {
-                     validMsg.classList.remove("valid-msg");
-                     validMsg.textContent = "Invalid email";
-                     validMsg.classList.add("error-msg");
+                     validationMessage.classList.remove("valid-msg");
+                     validationMessage.textContent = "Invalid email";
+                     validationMessage.classList.add("error-msg");
             }
         })
  
@@ -67,4 +76,14 @@ function validateEmail(email) {
 
     return emailRegex.test(email);
  
+}
+
+
+function resetViews() {
+      // Update views TODO: remove success screen
+        signUpScreen.classList.remove("hidden");
+        successScreen.classList.add("hidden");
+
+        // Clear input field value
+    newsletterEmail.value = "";
 }
