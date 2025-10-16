2dbcd60d77741ab8b61b147cdcbb7e69696ad090
Sayon-okai <sayon.study@gmail.com>
2025-10-16 00:15:49 +0000

    no media query -  js DOM content added and tested for further logic

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Get DOM Content
+let signUpScreen =  document.querySelector(".sign-up-screen");
+let errMessage = document.querySelector(".error-msg");
+let newsletterEmail = document.querySelector("#email");
+let subscribeButton = document.querySelector("#subscribe-btn");
+let successEmail = document.querySelector(".visitor-email");
+let dismissMsgBtn = document.querySelector("#dismiss-msg");
+let successScreen = document.querySelector(".success-screen");
+
+
+subscribeButton.addEventListener("click", function () {
+ 
+
+
+})
